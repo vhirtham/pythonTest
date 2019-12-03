4b16db31689a1ab0b7e45988903c7d4c0afe5891
Hirthammer <volker.hirthammer@bam.de>
2019-12-03 09:50:59 +0100

    Fix travis failure (hopefully)

--- tests/test_HelloWorld.py
+++ tests/test_HelloWorld.py
@@ -4,6 +4,7 @@ import sys
 
 filePath = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(filePath + '\\..\\MyPackages')
+sys.path.append(filePath + '/../MyPackages')
 
 
 from MyFuncs import *
