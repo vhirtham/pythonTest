308316cb35198b07d47042adaec43e15059a8a5c
Hirthammer <volker.hirthammer@bam.de>
2019-12-03 09:12:48 +0100

    Modify test

--- tests/test_HelloWorld.py
+++ tests/test_HelloWorld.py
@@ -1,2 +1,12 @@
-def test_HelloWorld():
+def MyFunc(bool):
+    if bool:
+        print("The answer is 42")
+    else:
+        print("This branch is not covered")
+
+
+def test_PrintHelloWorld():
     print('Hello world')
+
+def test_MyFunc():
+    MyFunc(True)
