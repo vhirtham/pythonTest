b6e6226ff9492fd2c15f27777ca2e612eb4e6366
Hirthammer <volker.hirthammer@bam.de>
2019-12-06 10:24:36 +0100

    Add doc-strings

--- MyPackages/MyFuncs.py
+++ MyPackages/MyFuncs.py
@@ -1,4 +1,14 @@
+"""
+Contains some test functions
+"""
+
+
 def MyFunc(bool):
+    """
+    Prints a message
+    :param bool: Selects one of two possible messages
+    :return: ---
+    """
     if bool:
         print("The answer is 42")
         print("Tvis is bat inglish")
@@ -6,6 +16,12 @@ def MyFunc(bool):
         print("This branch is not covered")
 
 def AddNumbers(a,b):
+    """
+    Adds 2 numbers
+    :param a: First number
+    :param b: Second number
+    :return: Result of the addition
+    """
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("First argument must be a integer or float")
     if not (isinstance(b, int) or isinstance(b, float)):
