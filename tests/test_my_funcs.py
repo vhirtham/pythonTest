cd582f49ad7ce3f488c5b86bba4d3b87aa8d5bc3
Hirthammer <volker.hirthammer@bam.de>
2019-12-11 17:05:37 +0100

    Add some asserts

--- tests/test_my_funcs.py
+++ tests/test_my_funcs.py
@@ -20,7 +20,9 @@ def test_my_func():
     mf.my_func(True)
 
 def test_add_numbers():
-    assert mf.add_numbers(1,2) == 3
+    assert mf.add_numbers(1, 2) == 3
+    assert mf.add_numbers(2, 1) == 3
+    assert mf.add_numbers(3, 2) == 5
     with pytest.raises(TypeError):
         mf.add_numbers("1",3)
 
