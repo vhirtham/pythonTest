c2200354aa401a9622f88df66dd5429f96bf9179
mnagel <markus.nagel@bam.de>
2019-12-18 17:55:53 +0100

    flake8

--- mypackage/mark_funcs.py
+++ mypackage/mark_funcs.py
@@ -8,25 +8,26 @@ import numpy as np
 def function_one():
     """
     Print "Hello World"!
-    
+
     :param: ---
     :return: ---
     """
     print("Hello World")
 
+
 def function_two():
     """
     Some stuff with Xarrays
-    
+
     :param: ---
     :return: ---
     """
-    xcoords=np.linspace(0,1,2)
-    ycoords=np.linspace(0,1,2)
+    xcoords = np.linspace(0, 1, 2)
+    ycoords = np.linspace(0, 1, 2)
     a = xr.DataArray(
-        [[True, False],[False,True]],
+        [[True, False], [False, True]],
         name=["xcoords", "ycoords"],
-        dims=["x","y"],
+        dims=["x", "y"],
         coords=dict(x=xcoords, y=ycoords),
     )
     try:
@@ -34,6 +35,5 @@ def function_two():
             raise KeyError
         else:
             a.loc[dict(x=1, y=1)] = True
-            
     except KeyError:
         print("Punkt schon vorhanden!")
--- tests/test_mark_funcs.py
+++ tests/test_mark_funcs.py
@@ -1,8 +1,9 @@
 import mypackage.mark_funcs as mf
-import pytest
+
 
 def test_function_one():
     mf.function_one()
+
 
 def test_function_two():
     mf.function_two()
