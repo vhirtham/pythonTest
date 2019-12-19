a14a565dba48c0f2ba7607be06914eb162bababd
Hirthammer <volker.hirthammer@bam.de>
2019-12-19 15:33:36 +0100

    Add segments to Shape2D

--- mypackage/geometry.py
+++ mypackage/geometry.py
@@ -18,33 +18,95 @@ def is_row_in_array(row, array):
 class Shape2D:
     """Defines a shape in 2 dimensions."""
 
-    def __init__(self, x0, y0, x1, y1):
-        """
-        Constructor.
+    # Member variables --------------------------------------------------------
 
-        :param x0: x-coordinate of first point
-        :param y0: y-coordinate of first point
-        :param x1: x-coordinate of second point
-        :param y1: y-coordinate of second point
-        """
-        self._points = np.array([[x0, y0], [x1, y1]])
+    min_segment_length = 1E-6
 
-    def is_point_included(self, point):
-        """
-        Check if a point is already part of the shape.
+    # Member classes ----------------------------------------------------------
 
-        :param point: Point which should be checked
-        :return: True or False
-        """
-        return is_row_in_array(point, self._points)
+    class Segment:
+        """Base class for segments."""
 
-    def is_shape_closed(self):
-        """
-        Check if the shape is already closed.
+        min_segment_length = 1E-6
 
-        :return: True or False
+        def check_valid(self, point_start, point_end):
+            """
+            Checks if the segments data is compatible with the passed start
+            and end points. Raises an Exception if not.
+
+            :param point_start: Starting point of the segment
+            :param point_end: End point of the segment
+            :return: ---
+            """
+            pass
+
+    class LineSegment(Segment):
+        """Line segment."""
+        pass
+
+    class ArcSegment(Segment):
+        """Segment of a circle."""
+
+        def __init__(self, center):
+            self._center = center
+
+    # Private methods ---------------------------------------------------------
+
+    def __init__(self, point0, point1, segment=LineSegment()):
         """
-        return is_row_in_array(self._points[-1, :], self._points[:-1])
+        Constructs the shape with an initial segment.
+
+        :param point0: first point
+        :param point1: second point
+        :param segment: segment
+        """
+        point0 = np.array(point0)
+        point1 = np.array(point1)
+
+        Shape2D._check_point_data_valid(point0)
+        Shape2D._check_point_data_valid(point1)
+        Shape2D._check_segment_length_valid(point0, point1)
+
+        self._points = np.array([point0, point1])
+        self._segments = []
+        self._append_segments(segment)
+
+    def _append_segments(self, segment):
+        """
+        Appends the internal segment array.
+
+        :param segment: segment that should be added
+        :return: ---
+        """
+        if Shape2D.is_segment_type_valid(segment):
+            self._segments.append(segment)
+        else:
+            raise ValueError("Unknown segment type")
+
+    def _check_segment_length_valid(point_start, point_end):
+        """
+        Checks if a segment is valid. Raises an Exception if not.
+
+        :param point_start: Starting point of the segment
+        :param point_end: End point of the segment
+        :return:
+        """
+        diff = point_start - point_end
+        if not np.dot(diff, diff) >= Shape2D.min_segment_length:
+            raise Exception("Segment length is too small.")
+
+    def _check_point_data_valid(point):
+        """
+        Checks if the data of a point is valid. Raises an Exception if not.
+
+        :param point: Point that should be checked
+        :return: ---
+        """
+        if not (np.ndim(point) == 1 and point.size == 2):
+            raise Exception(
+                "Point data is invalid. Must be an array with 2 values.")
+
+    # Public methods ----------------------------------------------------------
 
     def add_segment(self, x_coord, y_coord):
         """
@@ -59,3 +121,32 @@ class Shape2D:
             raise ValueError("Shape is already closed")
 
         self._points = np.vstack((self._points, point))
+
+    def is_point_included(self, point):
+        """
+        Check if a point is already part of the shape.
+
+        :param point: Point which should be checked
+        :return: True or False
+        """
+        return is_row_in_array(point, self._points)
+
+    def is_segment_type_valid(segment):
+        """
+        Checks if the passed segment type is valid
+
+        :param segment: segment
+        :return: True or False
+        """
+        if isinstance(segment, Shape2D.Segment):
+            return True
+        else:
+            return False
+
+    def is_shape_closed(self):
+        """
+        Check if the shape is already closed.
+
+        :return: True or False
+        """
+        return is_row_in_array(self._points[-1, :], self._points[:-1])
--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -2,8 +2,19 @@ import pytest
 import mypackage.geometry as geo
 
 
+def basic_segment_tests(segment_type, constructor_args):
+    # Segment length too small
+    with pytest.raises(Exception):
+        geo.Shape2D([0, 0], [0, 0], segment_type(*constructor_args))
+
+
+def test_class_arc_segment():
+    arc_segment = geo.Shape2D.ArcSegment(3)
+    shape = geo.Shape2D([0, 0], [1, 1], segment=arc_segment)
+
+
 def test_class_shape2d_point_operations():
-    shape = geo.Shape2D(0, 0, 0, 1)
+    shape = geo.Shape2D([0, 0], [0, 1])
     shape.add_segment(2, 2)
     shape.add_segment(1, 0)
     shape.add_segment(0, 0)
