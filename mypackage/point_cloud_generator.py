752659cb06e0e7bbdbdcefb058d13e852cadf47d
Hirthammer <volker.hirthammer@bam.de>
2020-01-09 10:56:40 +0100

    Refactor profile class

--- mypackage/point_cloud_generator.py
+++ mypackage/point_cloud_generator.py
@@ -1,6 +1,7 @@
 """Contains methods and classes to generate 3d point clouds."""
 
 import mypackage.geometry as geo
+import numpy as np
 
 
 class Profile:
@@ -12,25 +13,25 @@ class Profile:
 
         :param: shapes: Instance or list of geo.Shape2D class(es)
         """
+        self.shapes = []
+
+        self.add_shapes(shapes)
+
+    def add_shapes(self, shapes):
+        """
+        Add shapes to the profile.
+
+        :param shapes: Instance or list of geo.Shape2D class(es)
+        :return: ---
+        """
         if not isinstance(shapes, list):
             shapes = [shapes]
 
         if not all(isinstance(shape, geo.Shape2D) for shape in shapes):
-            raise ValueError(
+            raise TypeError(
                 "Only instances or lists of Shape2d objects are accepted.")
 
-        self.shapes = shapes
-
-    def add_shape(self, shape):
-        """
-        Add a shape to the profile.
-
-        :param shape: Shape that should be added.
-        :return: ---
-        """
-        if not isinstance(shape, geo.Shape2D):
-            raise ValueError("Only instances Shape2d objects are accepted.")
-        self.shapes.append(shape)
+        self.shapes += shapes
 
     def num_shapes(self):
         """
@@ -39,3 +40,17 @@ class Profile:
         :return: Number of shapes
         """
         return len(self.shapes)
+
+    def rasterize(self, raster_width):
+        """
+        Rasterize the profile.
+
+        :param: raster_width: Raster width
+        :return: Raster data
+        """
+        raster_data = np.empty([0, 3])
+        for shape in self.shapes:
+            raster_data = np.vstack(
+                (raster_data, shape.rasterize(raster_width)))
+
+        return raster_data
--- tests/test_point_cloud_generator.py
+++ tests/test_point_cloud_generator.py
@@ -9,19 +9,33 @@ def test_profile_construction_and_shape_addition():
     shape.add_segment([0, -1])
 
     # Check invalid types
-    with pytest.raises(ValueError):
+    with pytest.raises(TypeError):
         pcg.Profile(3)
-    with pytest.raises(ValueError):
+    with pytest.raises(TypeError):
         pcg.Profile("This is not right")
-    with pytest.raises(ValueError):
+    with pytest.raises(TypeError):
         pcg.Profile([2, 8, 1])
 
     # Check valid types
     profile = pcg.Profile(shape)
     assert profile.num_shapes() == 1
-
     profile = pcg.Profile([shape, shape])
     assert profile.num_shapes() == 2
 
-    profile.add_shape(shape)
+    # Check invalid addition
+    with pytest.raises(TypeError):
+        profile.add_shapes([shape, 0.1])
+    with pytest.raises(TypeError):
+        profile.add_shapes(["shape"])
+    with pytest.raises(TypeError):
+        profile.add_shapes(0.1)
+
+    # Check that invalid calls only raise an exception and do not invalidate
+    # the internal data
+    assert profile.num_shapes() == 2
+
+    # Check valid addition
+    profile.add_shapes(shape)
     assert profile.num_shapes() == 3
+    profile.add_shapes([shape, shape])
+    assert profile.num_shapes() == 5
