3c0c577f3b5bb699ffc0135003fb0f3a55113426
mnagel <markus.nagel@bam.de>
2020-02-12 15:25:19 +0100

    imperative mood is hard!!!

--- mypackage/all_groove.py
+++ mypackage/all_groove.py
@@ -10,7 +10,7 @@ def singleVGrooveButtWeld(
     t, alpha, b, c, width_default=Quantity(5, unit="millimeter")
 ):
     """
-    Calculates a Single-V Groove Butt Weld.
+    Calculate a Single-V Groove Butt Weld.
 
     :param t: the workpiece thickness, as Astropy unit
     :param alpha: the groove angle, as Astropy unit
@@ -56,7 +56,7 @@ def singleUGrooveButtWeld(
     t, beta, R, b, c, width_default=Quantity(15, unit="millimeter")
 ):
     """
-    Calculates a Single-U Groove Butt Weld.
+    Calculate a Single-U Groove Butt Weld.
 
     :param t: the workpiece thickness, as Astropy unit
     :param beta: the bevel angle, as Astropy unit
